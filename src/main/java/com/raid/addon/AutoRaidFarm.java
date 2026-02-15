9363824bbcf8dfd628490221298d1b86ddec38bc
bitsixfour <tugirasles@gmail.com>
2026-02-14 23:31:59 -0600

    test an intial module

--- src/main/java/com/raid/addon/AutoRaidFarm.java
+++ src/main/java/com/raid/addon/AutoRaidFarm.java
@@ -10,30 +10,25 @@ import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.fabricmc.loader.impl.util.log.Log;
 import net.minecraft.item.Items;
 import org.slf4j.Logger;
 
+
 public class AutoRaidFarm extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-
-
-
     @Override
     public void onInitialize() {
+        LOG.info("Initializing Meteor Villager Roller");
         Modules.get().add(new AutomateRaid());
-
     }
     @Override
     public void onRegisterCategories() {
-
     }
     @Override
     public String getPackage() {
         return "com.raid.addon";
     }
-
-
-
 
     @Override
     public GithubRepo getRepo() {
